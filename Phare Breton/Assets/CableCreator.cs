63ae92a1244316aae6204d3fcc7a0c64abfc6174
Arthur <arthur.perigno@gmail.com>
2023-03-08 12:45:18 +0100

    Phisique du câble FINIE

--- Phare Breton/Assets/CableCreator.cs
+++ Phare Breton/Assets/CableCreator.cs
@@ -7,9 +7,11 @@ using UnityEngine;
 
 public class CableCreator : MonoBehaviour
 {
-    [Header("Paramètres")]
-    [SerializeField] private int nbrNodes;
+    [Header("Paramètres")] 
+    [SerializeField] private int nbrMaxNodes;
     [SerializeField] private float distanceBetweenNodes;
+    [SerializeField] private float minDistSpring;
+    [SerializeField] private float maxDistSpring;
     [SerializeField] private float spring;
     [SerializeField] private float damper;
     
@@ -19,15 +21,23 @@ public class CableCreator : MonoBehaviour
     public GameObject end;
 
     private List<GameObject> nodesRope = new List<GameObject>();
+    private float currentLength;
+    private int nbrNodes;
 
-    
+
+    private void Update()
+    {
+        ActualiseCable();
+    }
 
 
     public void CreateNodes()
     {
         float distanceStartEnd = Vector3.Distance(origin.transform.position, end.transform.position);
         Vector3 directionStartEnd = end.transform.position - origin.transform.position;
-        
+
+        nbrNodes = (int) (distanceStartEnd / distanceBetweenNodes);
+
         nodesRope.Add(origin);
         
         for (int k = 0; k < nbrNodes; k++)
@@ -48,35 +58,106 @@ public class CableCreator : MonoBehaviour
     {
         for(int k = 1; k < nodesRope.Count - 1; k++)
         {
-            NodeCable currentNode = nodesRope[k].GetComponent<NodeCable>();
+            CreateLienBetweenNodes(k);
+        }
+    }
 
-            // CREATION DU LIEN
-            currentNode.node1 = nodesRope[k - 1].transform;
-            currentNode.node2 = nodesRope[k + 1].transform;
+    
+    private void CalculateCableLength()
+    {
+        currentLength = 0f;
+        
+        for(int k = 0; k < nodesRope.Count - 1; k++)
+        {
+            currentLength += Vector3.Distance(nodesRope[k].transform.position, nodesRope[k + 1].transform.position);
+        }
+    }
 
-            currentNode.spring1.connectedBody = nodesRope[k - 1].GetComponent<Rigidbody>();
-            currentNode.spring2.connectedBody = nodesRope[k + 1].GetComponent<Rigidbody>();
+
+    private void CreateNewNode()
+    {
+        Vector3 newPos = nodesRope[nodesRope.Count - 1].transform.position -
+                         nodesRope[nodesRope.Count - 2].transform.position;
+        
+        GameObject newNode = Instantiate(node, nodesRope[nodesRope.Count - 2].transform.position + newPos, Quaternion.identity, transform);
+        
+        // Placement dans la liste des nodes de la nouvelle node
+        nodesRope.Add(newNode);
+
+        GameObject saveNode = nodesRope[nodesRope.Count - 1];
+        nodesRope[nodesRope.Count - 1] = nodesRope[nodesRope.Count - 2];
+        nodesRope[nodesRope.Count - 2] = saveNode;
+        
+        
+        // Création du lien avec les nodes adjacentes
+        CreateLienBetweenNodes(nodesRope.Count - 2);
+        CreateLienBetweenNodes(nodesRope.Count - 3);
+    }
+    
+    
+    private void DestroyNewNode()
+    {
+        Destroy(nodesRope[nodesRope.Count - 2]);
+        nodesRope.RemoveAt(nodesRope.Count - 2);
+        
+        CreateLienBetweenNodes(nodesRope.Count - 2);
+    }
+
+    
+    private void CreateLienBetweenNodes(int index)
+    {
+        NodeCable currentNode = nodesRope[index].GetComponent<NodeCable>();
+
+        // CREATION DU LIEN
+        currentNode.node1 = nodesRope[index - 1].transform;
+        currentNode.node2 = nodesRope[index + 1].transform;
+
+        currentNode.spring1.connectedBody = nodesRope[index - 1].GetComponent<Rigidbody>();
+        currentNode.spring2.connectedBody = nodesRope[index + 1].GetComponent<Rigidbody>();
+
+        // GESTION DISTANCE ENTRE POINTS
+        currentNode.spring1.anchor = Vector3.zero;
+        currentNode.spring2.anchor = Vector3.zero;
+
+        Vector3 direction = nodesRope[index + 1].transform.position - currentNode.transform.position;
+        
+        currentNode.spring1.connectedAnchor = Vector3.zero;
+        currentNode.spring2.connectedAnchor = direction.normalized * 0.4f;
+        
+        /*currentNode.spring1.connectedAnchor = nodesRope[index - 1].transform.position;
+        currentNode.spring2.connectedAnchor = nodesRope[index + 1].transform.position;*/
+        
+        /*currentNode.spring1.connectedAnchor = nodesRope[index - 1].transform.position - nodesRope[index].transform.position;
+        currentNode.spring2.connectedAnchor = nodesRope[index + 1].transform.position - nodesRope[index].transform.position;*/
             
             
-            // GESTION DISTANCE ENTRE POINTS
-            currentNode.spring1.anchor = new Vector3(-distanceBetweenNodes, 0, 0);
-            currentNode.spring2.anchor = new Vector3(distanceBetweenNodes, 0, 0);
+        // GESTION PHYSIQUE CORDE
+        currentNode.spring1.spring = spring;
+        currentNode.spring2.spring = spring * 4;
             
-            currentNode.spring1.connectedAnchor = new Vector3(-distanceBetweenNodes * 2, 0, 0);
-            currentNode.spring2.connectedAnchor = new Vector3(distanceBetweenNodes * 2, 0, 0);
+        currentNode.spring1.damper = damper;
+        currentNode.spring2.damper = damper;
             
-            
-            // GESTION PHYSIQUE CORDE
-            /*currentNode.spring1.massScale = poidsCable;
-            currentNode.spring2.massScale = poidsCable;*/
-            
-            currentNode.spring1.spring = spring;
-            currentNode.spring2.spring = spring * 3;
-            
-            currentNode.spring1.damper = damper;
-            currentNode.spring2.damper = damper;
-            
-            currentNode.spring2.maxDistance = distanceBetweenNodes;
+        currentNode.spring1.minDistance = minDistSpring;
+        currentNode.spring2.minDistance = minDistSpring;
+        
+        currentNode.spring1.maxDistance = maxDistSpring;
+        currentNode.spring2.maxDistance = maxDistSpring;
+    }
+
+
+    private void ActualiseCable()
+    {
+        CalculateCableLength();
+
+        if (currentLength > nodesRope.Count * distanceBetweenNodes && nodesRope.Count < nbrMaxNodes)
+        {
+            CreateNewNode();
+        }
+        
+        else if (currentLength < (nodesRope.Count * distanceBetweenNodes) - distanceBetweenNodes)
+        {
+            DestroyNewNode();
         }
     }
 }
