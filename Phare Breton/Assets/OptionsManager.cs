31aa12eb437ef414e687aadc11dfc30f14a4b267
Arthur <arthur.perigno@gmail.com>
2023-05-28 17:25:01 +0200

    Options menu principal navigables

---
+++ Phare Breton/Assets/OptionsManager.cs
@@ -0,0 +1,225 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using DG.Tweening;
+using TMPro;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.UI;
+
+public class OptionsManager : MonoBehaviour
+{
+    [Header("Références")] 
+    [SerializeField] private GameObject optionsObject;
+    [SerializeField] private TextMeshProUGUI optionsMainText;
+    [SerializeField] private List<TextMeshProUGUI> texts = new List<TextMeshProUGUI>();
+    [SerializeField] private List<GameObject> sliders = new List<GameObject>();
+    [SerializeField] private MenuPrincManager menuPrincipalScript;
+    
+    [Header("Inputs")] 
+    private bool pause;
+    private bool interaction;
+    private bool up;
+    private bool down;
+    private bool escape;
+
+    [Header("Autres")] 
+    public int index = 0;
+    private bool canUse;
+
+    private void Start()
+    {
+        StartCoroutine(QuitOptions(0, 0));
+    }
+
+    private void Update()
+    {
+        if (canUse)
+        {
+            if (down || up)
+            {
+                ChangeSelected();
+
+                down = false;
+                up = false;
+            }
+
+            if (escape)
+            {
+                StartCoroutine(QuitOptions(1, 0));
+            }
+        }
+    }
+
+
+    public void ChangeSelected()
+    {
+        if (up)
+        {
+            if (index - 1 >= 0)
+            {
+                index -= 1;
+                
+                GoUp(0.3f, 1);
+            }
+        }
+        else
+        {
+            if (index + 1 < texts.Count)
+            {
+                index += 1;
+                
+                GoDown(0.3f, 1);
+            }
+        }
+    }
+
+    
+    public void GoUp(float duration, float value)
+    {
+        texts[index + 1].DOFade(0.5f, duration);
+        
+        texts[index].DOFade(value, duration);
+        
+        
+        Image[] sliderImages = sliders[index + 1].GetComponentsInChildren<Image>();
+
+        for (int k = 0; k < sliderImages.Length; k++)
+        {
+            sliderImages[k].DOFade(0.5f, duration);
+        }
+        
+        sliderImages = sliders[index].GetComponentsInChildren<Image>();
+
+        for (int k = 0; k < sliderImages.Length; k++)
+        {
+            sliderImages[k].DOFade(value, duration);
+        }
+    }
+
+    
+    public void GoDown(float duration, float value)
+    {
+        texts[index - 1].DOFade(0.5f, duration);
+        
+        texts[index].DOFade(value, duration);
+        
+        
+        Image[] sliderImages = sliders[index - 1].GetComponentsInChildren<Image>();
+
+        for (int k = 0; k < sliderImages.Length; k++)
+        {
+            sliderImages[k].DOFade(0.5f, duration);
+        }
+        
+        sliderImages = sliders[index].GetComponentsInChildren<Image>();
+
+        for (int k = 0; k < sliderImages.Length; k++)
+        {
+            sliderImages[k].DOFade(value, duration);
+        }
+    }
+    
+    
+    
+    public IEnumerator OpenOptions(float duration, float value)
+    {
+        index = 0;
+        
+        optionsMainText.DOFade(value, duration);
+        
+        for (int i = 0; i < texts.Count; i++)
+        {
+            texts[i].DOFade(value, duration);
+        }
+        
+        for (int i = 0; i < sliders.Count; i++)
+        {
+            Image[] sliderImages = sliders[i].GetComponentsInChildren<Image>();
+
+            for (int k = 0; k < sliderImages.Length; k++)
+            {
+                sliderImages[k].DOFade(value, duration);
+            }
+        }
+
+        optionsObject.SetActive(true);
+
+        menuPrincipalScript.noControl = true;
+        
+        GoUp(duration, 1);
+
+        yield return new WaitForSeconds(duration);
+
+        canUse = true;
+    }
+
+    
+    public IEnumerator QuitOptions(float duration, float value)
+    {
+        canUse = false;
+        
+        optionsMainText.DOFade(value, duration);
+        
+        for (int i = 0; i < texts.Count; i++)
+        {
+            texts[i].DOFade(value, duration);
+        }
+        
+        for (int i = 0; i < sliders.Count; i++)
+        {
+            Image[] sliderImages = sliders[i].GetComponentsInChildren<Image>();
+
+            for (int k = 0; k < sliderImages.Length; k++)
+            {
+                sliderImages[k].DOFade(value, duration);
+            }
+        }
+
+        yield return new WaitForSeconds(duration);
+        
+        optionsObject.SetActive(false);
+
+        menuPrincipalScript.noControl = false;
+    }
+    
+    
+    // ----------------------------------------------------------------------
+    // INPUTS
+
+    public void OnInteraction(InputAction.CallbackContext context)
+    {
+        if (context.started)
+            interaction = true;
+
+        if (context.canceled)
+            interaction = false;
+    }
+    
+    public void OnUp(InputAction.CallbackContext context)
+    {
+        if (context.started)
+            up = true;
+
+        if (context.canceled)
+            up = false;
+    }
+    
+    public void OnDown(InputAction.CallbackContext context)
+    {
+        if (context.started)
+            down = true;
+
+        if (context.canceled)
+            down = false;
+    }
+    
+    public void OnEscape(InputAction.CallbackContext context)
+    {
+        if (context.started)
+            escape = true;
+
+        if (context.canceled)
+            escape = false;
+    }
+}
