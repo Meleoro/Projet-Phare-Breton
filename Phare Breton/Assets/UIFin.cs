dd48fb0794a3d3bac54799effb1a4c18bf2f6d69
Arthur <arthur.perigno@gmail.com>
2023-06-04 08:43:58 +0200

    Les credits sont présents, si ça c'est pas beau

--- Phare Breton/Assets/UIFin.cs
+++ Phare Breton/Assets/UIFin.cs
@@ -3,11 +3,17 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using DG.Tweening;
+using TMPro;
 
 public class UIFin : MonoBehaviour
 {
     public Image titre;
     public Image triangle;
+
+    public RectTransform pos2;
+
+    public List<TextMeshProUGUI> names = new List<TextMeshProUGUI>();
+
 
 
     private void Start()
@@ -27,6 +33,18 @@ public class UIFin : MonoBehaviour
 
         triangle.rectTransform.DORotate(new Vector3(0, 0, 180), 2).SetEase(Ease.InOutSine);
 
-        yield return new WaitForSeconds(3);
+        yield return new WaitForSeconds(2.5f);
+
+        titre.rectTransform.DOMoveY(pos2.position.y, 1.5f);
+        triangle.rectTransform.DOMoveY(pos2.position.y, 1.5f);
+
+        yield return new WaitForSeconds(1.3f);
+
+        for (int i = 0; i < names.Count; i++)
+        {
+            names[i].DOFade(1, 2);
+        }
+        
+        yield return new WaitForSeconds(10);
     }
 }
