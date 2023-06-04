f821dff4a10cfac63cfb352d1f454fa2930781df
Arthur <arthur.perigno@gmail.com>
2023-06-04 16:46:09 +0200

    Taille objets recuperables

---
+++ Phare Breton/Assets/ObjetRecuperable.cs
@@ -0,0 +1,8 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjetRecuperable : MonoBehaviour
+{
+    [Range(2, 10)] public float distanceObjet;
+}
